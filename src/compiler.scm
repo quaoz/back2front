eee2e32f8846b50d0be1b3c6c4d1a7abcec75c10
quaoz <74419801+quaoz@users.noreply.github.com>
2024-10-29 19:14:20 +0000

    1.2: Immediate Constants

--- src/compiler.scm
+++ src/compiler.scm
@@ -1,17 +1,53 @@
+; place compiled output in build dir
 (if (not (file-exists? "build"))
     (mkdir "build"))
-
 (cd "./build")
 
+; include tests
 (source-directories '("../tests" "../src"))
 (load "../tests/tests-driver.scm")
 (load "../tests/tests-1.1-req.scm")
+(load "../tests/tests-1.2-req.scm")
+
+; scheme constants
+(define fx_shift       2)
+(define fx_mask     #x03)
+(define fx_tag      #x00)
+(define bool_f      #x2F)
+(define bool_t      #x6F)
+(define empty_list  #x3F)
+(define char_shift     8)
+(define char_mask   #x0F)
+(define char_tag    #x0F)
+(define wordsize       4)
+
+; 30 bits for number, 2 for tag
+(define fixnum-bits (- (* wordsize 8) fx_shift))
+
+; highest and lowest numbers we can represent with fixnum-bits
+(define fx_upper (sub1 (expt 2 (- fixnum-bits 1))))
+(define fx_lower (- (expt 2 (- fixnum-bits 1))))
+
+(define (fixnum? x)
+    (and (integer? x) (exact? x) (<= fx_lower x fx_upper)))
+
+(define (immediate? x)
+    (or (fixnum? x) (boolean? x) (null? x) (char? x)))
+
+(define (immediate-rep x)
+    (cond
+     [(fixnum? x) (ash x fx_shift)]
+     [(boolean? x) (if x bool_t bool_f)]
+     [(null? x) empty_list]
+     [(char? x) (bitwise-ior (ash (char->integer x) char_shift) char_tag)]
+     [else #f]
+    ))
 
 (define (emit-program x)
-  (unless (integer? x) (error 'compile-program "not an integer"))
+  (unless (immediate? x) (error 'emit-program (format "'~s' not an immediate" x)))
   (emit "	.text")
   (emit "   .balign 4")
   (emit "	.globl _scheme_entry")
   (emit "_scheme_entry:")
-  (emit "	mov w0, ~s" x)
+  (emit "	mov w0, ~s" (immediate-rep x))
   (emit "	ret"))
--- src/startup.c
+++ src/startup.c
@@ -1,8 +1,56 @@
 #include <stdio.h>
 
-extern int scheme_entry();
+// scheme constants
+#define fx_shift 2
+#define fx_mask 0x03
+#define fx_tag 0x00
+#define bool_f 0x2F
+#define bool_t 0x6F
+#define empty_list 0x3F
+#define char_shift 8
+#define char_mask 0x0F
+#define char_tag 0x0F
 
-int main(int argc, char** argv){
-  printf("%d\n", scheme_entry());
+typedef unsigned int ptr;
+
+extern ptr scheme_entry();
+
+static void print_ptr(ptr x) {
+  if ((x & fx_mask) == fx_tag) {
+    printf("%d", ((int)x) >> fx_shift);
+  } else if (x == bool_f) {
+    printf("#f");
+  } else if (x == bool_t) {
+    printf("#t");
+  } else if (x == empty_list) {
+    printf("()");
+  } else if ((x & char_mask) == char_tag) {
+    char c = (char)(x >> char_shift);
+
+    switch (c) {
+    case '\t':
+      printf("#\\tab");
+      break;
+    case '\n':
+      printf("#\\newline");
+      break;
+    case '\r':
+      printf("#\\return");
+      break;
+    case ' ':
+      printf("#\\space");
+      break;
+    default:
+      printf("#\\%c", c);
+      break;
+    }
+  } else {
+    printf("#<unknown 0x%08x>", x);
+  }
+  printf("\n");
+}
+
+int main(int argc, char **argv) {
+  print_ptr(scheme_entry());
   return 0;
 }
